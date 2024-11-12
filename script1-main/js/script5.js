d357f603b7a91a947f6faf70be8dc9d87ceb7da8
boberSer <filippi.danila08@gmail.com>
2024-11-12 23:28:25 +0700

    coming

--- script1-main/js/script5.js
+++ script1-main/js/script5.js
@@ -3,14 +3,15 @@ let pos2 = document.querySelector('.reviews-item._second');
 let pos3 = document.querySelector('.reviews-item._third');
 let pos4 = document.querySelector('.reviews-item._fourth');
 let button = document.querySelector('.reviews-item__btn')
-let grid = document.querySelector('.reviews-list');
-let reviews = document.querySelectorAll('.reviews-item');
+let grid = document.querySelector('.reviews__list');
+let reviewsItem = document.querySelectorAll('.reviews-item');
+
 
 const REVIEW_CLASSES = ['_first', '_second', '_third', '_fourth'];
 
 const reviewItems = [pos1, pos2, pos3, pos4];
 
-reviews.forEach((item) => {
+reviewsItem.forEach((item) => {
     item.onclick = () => {
         reviewItems.unshift(reviewItems.pop())
         reviewItems.forEach((item, index) => {
@@ -21,7 +22,13 @@ reviews.forEach((item) => {
 })
 
 button.addEventListener('click', function() {
-
+    grid.style.display = 'grid'
+    grid.style.gridTemplateColumns = "1fr 1fr"
+    reviewsItem.forEach((item) => {
+        item.classList.remove('reviews-item')
+        item.classList.add('grid-item')
+    })
+    button.style.display = 'none'
 })
 
 
